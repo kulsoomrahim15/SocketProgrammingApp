dfa513327e8d2ff93fdf3104ec85b8710ce4b167
kulsoom.rahim <kulsoom.rahim@DEV-KULSOOM>
2023-02-22 08:45:13 +0500

    Initial Commit

---
+++ Client/ClientSide/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main(string[] args)
+    {
+        string ipAddress = "127.0.0.1";
+        int port = 8080;
+
+        TcpClient client = new TcpClient(ipAddress, port);
+        Console.WriteLine("Connected to server");
+
+        NetworkStream stream = client.GetStream();
+
+        string message = "Hello from the client";
+        byte[] data = Encoding.ASCII.GetBytes(message);
+        stream.Write(data, 0, data.Length);
+        Console.WriteLine("Sent: {0}", message);
+
+        data = new byte[256];
+        int bytes = stream.Read(data, 0, data.Length);
+        message = Encoding.ASCII.GetString(data, 0, bytes);
+        Console.WriteLine("Received: {0}", message);
+
+        client.Close();
+    }
+}
---
+++ Server/Server_Side/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Server
+{
+    static void Main(string[] args)
+    {
+        IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+        int port = 8080;
+
+        TcpListener listener = new TcpListener(ipAddress, port);
+        listener.Start();
+        Console.WriteLine("Server is listening on {0}:{1}", ipAddress, port);
+
+        TcpClient client = listener.AcceptTcpClient();
+        Console.WriteLine("Client connected");
+
+        NetworkStream stream = client.GetStream();
+
+        byte[] data = new byte[256];
+        int bytes = stream.Read(data, 0, data.Length);
+        string message = Encoding.ASCII.GetString(data, 0, bytes);
+        Console.WriteLine("Received: {0}", message);
+
+        message = "Hello from the server";
+        data = Encoding.ASCII.GetBytes(message);
+        stream.Write(data, 0, data.Length);
+        Console.WriteLine("Sent: {0}", message);
+
+        client.Close();
+        listener.Stop();
+    }
+}
